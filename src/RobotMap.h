dc7d06594e707284728a04285457bdbeb9171760
justingriggs <justingriggs@10.15.152.250>
2017-02-11 18:48:36 -0600

    updated Motor Layout.  Shooter is on a 40A breaker now.  Intake Ball Motor moved.

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -26,20 +26,19 @@ constexpr int DRIVE_RIGHTMOTOR_1	= 0;
 constexpr int DRIVE_RIGHTMOTOR_2 	= 1;
 constexpr int DRIVE_RIGHTMOTOR_3 	= 2;
 
-constexpr int SHOOTER_MOTOR_1 		= 6;
-constexpr int SHOOTER_MOTOR_2 		= 7;
+constexpr int SHOOTER_MOTOR_1 		= 3;
+constexpr int SHOOTER_MOTOR_2 		= 11;
 
-constexpr int CONVEYOR_LOWER_MOTOR  = 99;
-constexpr int CONVEYOR_UPPER_MOTOR  = 99;
+constexpr int CONVEYOR_LOWER_MOTOR  = 4;
+constexpr int CONVEYOR_UPPER_MOTOR  = 5;
 
-constexpr int INTAKE_BALL_MOTOR_1	= 11;
-//constexpr int INTAKE_BALL_MOTOR_2	= 5;
+constexpr int INTAKE_BALL_MOTOR_1	= 14;
 constexpr int INTAKE_GEAR_MOTOR		= 12;
 constexpr int INTAKE_ARM_MOTOR		= 13;
 constexpr int INTAKE_ARM_DOWN_LIMIT = 0;
 constexpr int INTAKE_ARM_UP_LIMIT	= 1;
 
-constexpr int TURRET_MOTOR 			= 99;
+constexpr int TURRET_MOTOR 			= 6;
 constexpr int TURRET_MIN_SWITCH		= 0;
 constexpr int TURRET_MAX_SWITCH		= 1;
 constexpr int TURRET_HOME_SWITCH	= 2;
