1ea41a37b26080dc5a74a7e2bcc8a1046c7b8386
justin <busted240sx@gmail.com>
2017-02-14 21:25:46 -0600

    added End() to TankDrive Command

--- src/Commands/TankDriveJoystick.cpp
+++ src/Commands/TankDriveJoystick.cpp
@@ -23,11 +23,11 @@ bool TankDriveJoystick::IsFinished() {
 
 // Called once after isFinished returns true
 void TankDriveJoystick::End() {
-
+	Drivetrain::GetInstance()->Tank(0,0);
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void TankDriveJoystick::Interrupted() {
-
+	End();
 }
