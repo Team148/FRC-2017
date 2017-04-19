6de82b4a42ca5adbb1dadbc1bff49d43de828a2d
james_tonthat <james_tonthat@GVO-KWF2.innovationfirst.com>
2017-04-19 08:58:11 -0500

    Start of Command Based AutoAim

--- src/Commands/AutoAim.cpp
+++ src/Commands/AutoAim.cpp
@@ -5,11 +5,15 @@
 AutoAim::AutoAim() {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
-//	Requires(Turret::GetInstance());
+	Requires(Turret::GetInstance());
+	Requires(Shooter::GetInstance());
+	m_isFinished = false;
+	SetTimeout(2.0);
 }
 
 // Called just before this Command runs the first time
 void AutoAim::Initialize() {
+	Shooter::GetInstance()->SetRingLightOn(true);
 	Turret::GetInstance()->UpdateVisionTarget();
 	m_yaw_offset = Turret::GetInstance()->GetYawOffset();
 	m_yaw_final = Turret::GetInstance()->GetBigAngle() + m_yaw_offset;
@@ -20,13 +24,13 @@ void AutoAim::Initialize() {
 void AutoAim::Execute() {
 	float error = Turret::GetInstance()->GetBigAngle() - m_yaw_final;
 
-	if(abs(error < 0.5))
+	if(abs(error < 0.5))	//when error is <0.50
 		m_isFinished = true;
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool AutoAim::IsFinished() {
-	return m_isFinished;
+	return m_isFinished ||IsTimedOut();
 }
 
 // Called once after isFinished returns true
